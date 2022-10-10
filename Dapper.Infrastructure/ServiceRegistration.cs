a7675d54bd91b9062af95cfbfb027a60929264b9
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-10-10 13:16:10 +0300

    Create ServiceRegistration.cs

---
+++ Dapper.Infrastructure/ServiceRegistration.cs
@@ -0,0 +1,19 @@
+﻿using Dapper.Application.Interfaces;
+using Dapper.Infrastructure.Context;
+using Dapper.Infrastructure.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Dapper.Infrastructure
+{
+    public static class ServiceRegistration
+    {
+       public static void AddInfrastructure(this IServiceCollection services)
+        {
+            services.AddScoped<DapperContext>();
+
+            services.AddTransient<IProductRepository, ProductRepository>();
+            services.AddTransient<IUnitOfWork, UnitOfWork>();
+
+        }
+    }
+}
